5ef6d87377fc915a2b48ab6aecec2c43c54b9cad
Sourav <souravberaofficial@gmail.com>
2024-03-23 00:39:38 +0530

    final commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import AppStateProvider from "@/components/providers/state-provider";
+import { Toaster } from "@/components/ui/toaster";
+import { SupabaseUserProvider } from "@/components/providers/supabase-user-provider";
+
+const font = Open_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "APEX",
+  description:
+    "AI Powered Collaborative Learning and Project Development Platform for Developers and Professionals.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          storageKey="apex-theme"
+        >
+          <AppStateProvider>
+            <SupabaseUserProvider>
+              {children}
+              <Toaster />
+            </SupabaseUserProvider>
+          </AppStateProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/mockdata/index.ts
@@ -0,0 +1,69 @@
+import { PassThrough } from "stream";
+
+export let IS_USER_SESSION = false;
+
+export let IS_FIRST_TIME = true;
+
+export const GROUP_DETAILS = {
+  environment_name: "Blazing AI",
+  profile: "/boardApe.png",
+  badge: "model_maestro",
+  projects_completed: 68,
+};
+export const USER = {
+  id: "1",
+  email: "vishal.test@gmail.com",
+  password: "123123",
+};
+
+export const USER_ONBOARDING_DETAILS = {
+  username: "Vishal",
+  firstName: "Vishal",
+  email: "vishal@gmail.com",
+  lastName: "M",
+  profilePicture: "/boardApe.png",
+  headline: "CEO of earth",
+  hasExperience: true,
+  education: [
+    {
+      school: "Name of School",
+      address: "Address",
+      course:
+        "Course Name",
+      from: "from",
+      to: "to",
+    },
+  ],
+  experience: [
+    {
+      position: "Position Title",
+      company: "Company ",
+      description:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit facere distinctio vel aspernatur veritatis enim voluptate nisi assumenda delectus ipsum, esse ad pariatur, qui natus tempora, saepe facilis ut sapiente.",
+      from: "from",
+      to: "to",
+    },
+  ],
+  address: "Address",
+  about:
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit facere distinctio vel aspernatur veritatis enim voluptate nisi assumenda delectus ipsum, esse ad pariatur, qui natus tempora, saepe facilis ut sapiente.",
+  links: [
+    { GitHub: "https://github.com/Mvishal123" },
+    { LinkedIn: "https://linkedin.com/Vishal123" },
+    { X: "https://x.com/Vishal123" },
+  ],
+  projectsList: [
+    {
+      name: "Project Name",
+      live: "https://livelink.com",
+      github: "https://github.com",
+      description:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit facere distinctio vel aspernatur veritatis enim voluptate nisi assumenda delectus ipsum, esse ad pariatur, qui natus tempora, saepe facilis ut sapiente.",
+    },
+  ],
+  skills: [
+    "Skill 1",
+    "Skill 2",
+    "Skill 3",
+  ],
+};
